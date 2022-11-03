373f676c6ac2694e3549566ded7ee50be6505a04
letonio <letoniojs@gmail.com>
2022-11-03 10:15:48 -0300

    upd: Add Module06 - Aula 07 e photoshop

--- Module06-JS/Aula07/Q2/index.js
+++ Module06-JS/Aula07/Q2/index.js
@@ -1,7 +1,7 @@
-let object = {
-    "Um atributo com espaços":1
+let obj = {
+	"Um atributo com espaços": 1,
 };
-console.log(object);
-console.log("Q2a - com colchetes ",object["Um atributo com espaços"]);
+console.log(obj);
+console.log("Q2a - com colchetes:\n", obj["Um atributo com espaços"]);
 //deu erro o segundo letra b
-//console.log("Q2b - sem colchetes ",object."Um atributo com espaços");
+console.log("Q2b - sem colchetes usando methodo values:\n", Object.values(obj));
